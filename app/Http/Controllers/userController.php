5be99566b65a29932805852543b2d1ff8038fc80
Moshtafizur-Rahman-Anty <alphaaa.90@gmail.com>
2025-05-06 18:38:48 +0600

    added authentications

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -4,17 +4,53 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
-class userController extends Controller
+class UserController extends Controller
 {
     //
+
+
+    public function showCorrectHomepage() {
+        if(auth()->check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
+
+
+    public function login(Request $request) {
+
+        $incomingRequest = $request->validate(
+
+
+            [
+                'loginusername' => 'required',
+                'loginpassword' => 'required'
+            ]
+
+            );
+
+            if(auth()->attempt(['username' => $incomingRequest['loginusername'], 'password' => $incomingRequest['loginpassword']])) {
+
+                $request->session()->regenerate();
+                return "Hello";
+            }
+
+            else {
+                return "Failed";
+            }
+
+
+    }
 
     public function register(Request $request) {
 
         $incomingRequest = $request->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed']
         ]);
 
         User::create($incomingRequest);
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExampleController;
 use App\Http\Controllers\userController;
 
-Route::get('/', [ExampleController::class, "homePage"]);
+Route::get('/', [userController::class, "showCorrectHomepage"]);
 Route::get('/about-us', [ExampleController::class, "aboutPage"]);
 Route::get('/single-post', [ExampleController::class, "singlePost"]);
 
 
+
+
 Route::post('/register', [userController::class, "register"]);
+Route::post('/login', [userController::class, "login"]);
